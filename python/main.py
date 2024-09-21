243ac265247646e3d1febca36253121bf7889bed
Shintaro Sakoda <sakoda@keio.jp>
2024-09-21 06:58:07 +0000

    Fixed main.py
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- python/main.py
+++ python/main.py
@@ -1,6 +1,9 @@
+"""Test the environment with a random agent."""
+
+import logging
+
 import gym
 import minerl  # noqa: F401
-import logging
 
 logging.basicConfig(level=logging.DEBUG)
 env = gym.make("MineRLObtainDiamondShovel-v0")
@@ -12,4 +15,8 @@ while not done:
     action = env.action_space.sample()
     action["ESC"] = 0
     obs, reward, done, _ = env.step(action)
+    print(obs.keys())
+    print(obs["pov"].shape, obs["pov"].dtype)
+    print(action)
     env.render()
+    break
