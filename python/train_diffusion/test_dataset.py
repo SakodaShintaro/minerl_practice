275390d398a4c0213efe59b7447148dbb522750a
Shintaro Sakoda <sakoda@keio.jp>
2024-09-23 01:55:55 +0000

    Fixed dataset
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

---
+++ python/train_diffusion/test_dataset.py
@@ -0,0 +1,46 @@
+"""Test the MineRLDataset class."""
+
+import argparse
+from pathlib import Path
+
+from minerl_dataset import MineRLDataset
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from train import center_crop_arr
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("root_dir", type=Path)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    root_dir = args.root_dir
+
+    transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], inplace=True),
+        ],
+    )
+    dataset = MineRLDataset(root_dir, transform=transform)
+    loader = DataLoader(
+        dataset,
+        batch_size=8,
+        shuffle=False,
+        num_workers=1,
+        pin_memory=True,
+        drop_last=True,
+    )
+
+    print(len(dataset))
+    print(dataset[0])
+
+    for batch in loader:
+        print(type(batch), len(batch))
+        image, action = batch
+        print(image.shape, image.dtype, image.min(), image.max())
+        print(action.shape, action.dtype, action.min(), action.max())
+        break
