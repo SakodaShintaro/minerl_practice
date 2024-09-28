ba80b96894b8a9ded878cf90d003e870e5f4c6c8
Shintaro Sakoda <sakoda@keio.jp>
2024-09-28 07:26:11 +0000

    Added train.sh
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

---
+++ python/train_diffusion/train.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -eux
+
+script_dir=$(readlink -f $(dirname $0))
+cd ${script_dir}
+
+result_dir=${script_dir}/../../train_result/result_$(date +"%Y%m%d_%H%M%S")
+
+MODEL_NAME="DiT-S/2"
+DATA_PATH="../../data"
+
+python3 train.py \
+  --model=${MODEL_NAME} \
+  --data-path=${DATA_PATH} \
+  --results-dir=${result_dir} \
+  --epochs=10 \
+  --global-batch-size=16 \
+  --ckpt-every=3000
+
+# checkpointディレクトリの中から最新のものを指定
+ckpt_id=$(ls ${result_dir}/checkpoints | sort | tail -n 1)
+
+python3 sample.py \
+  --data-path=${DATA_PATH} \
+  --ckpt=${result_dir}/checkpoints/${ckpt_id}
