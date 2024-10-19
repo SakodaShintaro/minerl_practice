14a58194d051a2510a60dfd7f6a6be68d2b2966d
Shintaro Sakoda <sakoda@keio.jp>
2024-10-19 08:02:20 +0000

    Added sample_flow_matching.py
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- python/train_flow_matching/train_flow_matching.py
+++ python/train_flow_matching/train_flow_matching.py
@@ -17,6 +17,7 @@ import torch
 from diffusers.models import AutoencoderKL
 from minerl_dataset import MineRLDataset
 from models import DiT_models
+from sample_flow_matching import sample_images
 from torch.utils.data import DataLoader
 from torchvision.utils import save_image
 
@@ -59,63 +60,6 @@ def requires_grad(model: torch.nn.Module, flag: bool) -> None:  # noqa: FBT001
     """Set requires_grad flag for all parameters in a model."""
     for p in model.parameters():
         p.requires_grad = flag
-
-
-def sample_images(
-    loader: DataLoader,
-    model: torch.nn.Module,
-    vae: AutoencoderKL,
-    args: argparse.Namespace,
-) -> torch.Tensor:
-    image_size = args.image_size
-    sample_n = args.nfe
-    with torch.no_grad():
-        device = model.parameters().__next__().device
-        latent_size = image_size // 8
-
-        results = []
-
-        for batch in loader:
-            image, action = batch
-            image = image.to(device)  # [b, seq, c, h, w]
-            gt_image = image[:, -1]
-            action = action.to(device)  # [b, seq, action_dim]
-            b, seq, c, h, w = image.shape
-            hidden_h = h // 8
-            hidden_w = w // 8
-            # Map input images to latent space + normalize latents:
-            image = image.view(b * seq, c, h, w)
-            image = vae.encode(image).latent_dist.sample().mul_(0.18215)
-            image = image.view(b, seq, 4, hidden_h, hidden_w)
-
-            cond_image = image[:, :-1]
-            cond_action = action[:, :-1]
-
-            # Create sampling noise:
-            z = torch.randn(b, 1, 4, latent_size, latent_size, device=device)
-
-            # Setup classifier-free guidance:
-            z = torch.cat([z, z], 0)
-            cond_image = torch.cat([cond_image, cond_image], 0)
-            cond_action = torch.cat([cond_action, cond_action], 0)
-
-            dt = 1.0 / sample_n
-            for i in range(sample_n):
-                num_t = i / sample_n * (1 - eps) + eps
-                t = torch.ones(b, device=device) * num_t
-                t = torch.cat([t, t], 0)
-                pred = model.forward(z, t * 999, cond_image, cond_action)
-                cond, uncond = pred.chunk(2, 0)
-                pred = uncond + (cond - uncond) * args.cfg_scale
-                pred = torch.cat([pred, pred], 0)
-                z = z.detach().clone() + pred * dt
-
-            samples = z[:b, 0]
-            pred_image = vae.decode(samples / 0.18215).sample
-            results.append((pred_image, gt_image, action))
-            break
-
-        return results
 
 
 def save_ckpt(  # noqa: PLR0913
