8bf67fa0535e79c9444aa702513e1e7b2d71fa09
Shintaro Sakoda <sakoda@keio.jp>
2024-11-02 03:01:57 +0000

    Added plot_loss_from_tsv.py
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

---
+++ python/train_diffusion/plot_loss_from_tsv.py
@@ -0,0 +1,28 @@
+"""log.tsvから損失の推移をプロットするスクリプト."""
+
+import argparse
+from pathlib import Path
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("log_path", type=Path)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    log_path = args.log_path
+    df = pd.read_csv(log_path, sep="\t")
+
+    print(df.head())
+
+    plt.plot(df["step"], df["loss"])
+    plt.xlabel("Step")
+    plt.ylabel("Loss")
+    save_path = log_path.parent / "step_loss.png"
+    plt.savefig(save_path, bbox_inches="tight", pad_inches=0.05)
+    print(f"Save plot to {save_path}")
