6a3cf5e2566ce576d164a2866305c919dd36e2ef
Shintaro Sakoda <sakoda@keio.jp>
2024-11-09 23:22:28 +0000

    Fixed to use train_stream
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- python/train_diffusion/train.sh
+++ python/train_diffusion/train.sh
@@ -9,7 +9,7 @@ result_dir=${script_dir}/../../train_result/$(date +"%Y%m%d_%H%M%S")
 MODEL_NAME="DiT-S/2"
 DATA_PATH="../../data"
 
-python3 train.py \
+python3 train_stream.py \
   --model=${MODEL_NAME} \
   --data_path=${DATA_PATH} \
   --results_dir=${result_dir}
