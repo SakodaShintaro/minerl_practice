af78cf2e957b7671b1ddf5a67d665ca51d9d4c1a
Shintaro Sakoda <sakoda@keio.jp>
2024-11-24 00:28:48 +0000

    Renamed
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- python/train_diffusion/train_online_rl.sh
+++ python/train_diffusion/train_online_rl.sh
@@ -8,7 +8,7 @@ result_dir=${script_dir}/../../train_result/$(date +"%Y%m%d_%H%M%S")_online_rl
 
 MODEL_NAME="DiT-S/2"
 
-python3 main.py \
+python3 train_online_rl.py \
   --model=${MODEL_NAME} \
   --results_dir=${result_dir}
 
