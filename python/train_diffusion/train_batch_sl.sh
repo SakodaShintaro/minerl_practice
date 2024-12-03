910b1b060139122ccc485e61fb8d040f3f07fa0a
Shintaro Sakoda <sakoda@keio.jp>
2024-12-03 23:38:47 +0000

    Fixed train_batch_sl.py
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

---
+++ python/train_diffusion/train_batch_sl.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -eux
+
+script_dir=$(readlink -f $(dirname $0))
+cd ${script_dir}
+
+result_dir=${script_dir}/../../train_result/$(date +"%Y%m%d_%H%M%S")_supervised
+
+MODEL_NAME="DiT-S/2"
+DATA_PATH="../../data"
+
+python3 train_batch_sl.py \
+  --model=${MODEL_NAME} \
+  --data_path=${DATA_PATH} \
+  --results_dir=${result_dir}
+
+python3 plot_images.py ${result_dir}
+python3 plot_loss_from_tsv.py ${result_dir}/log.tsv
+
+python3 evaluate_trained_model.py ${result_dir}/checkpoints/00100000.pt --nfe 64
+python3 evaluate_trained_model.py ${result_dir}/checkpoints/00100000.pt --nfe 16
+python3 evaluate_trained_model.py ${result_dir}/checkpoints/00100000.pt --nfe 4
+python3 evaluate_trained_model.py ${result_dir}/checkpoints/00100000.pt --nfe 1
+python3 plot_images_each_nfe.py ${result_dir}
