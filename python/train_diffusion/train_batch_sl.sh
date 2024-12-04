6344db0dcf3a5b6861b9654c78ca3421fbc8cfe8
Shintaro Sakoda <sakoda@keio.jp>
2024-12-04 01:18:34 +0000

    Added exit
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

--- python/train_diffusion/train_batch_sl.sh
+++ python/train_diffusion/train_batch_sl.sh
@@ -17,6 +17,8 @@ python3 train_batch_sl.py \
 python3 plot_images.py ${result_dir}
 python3 plot_loss_from_tsv.py ${result_dir}/log.tsv
 
+exit 0
+
 python3 evaluate_trained_model.py ${result_dir}/checkpoints/00100000.pt --nfe 64
 python3 evaluate_trained_model.py ${result_dir}/checkpoints/00100000.pt --nfe 16
 python3 evaluate_trained_model.py ${result_dir}/checkpoints/00100000.pt --nfe 4
